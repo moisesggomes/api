142442af0d7c24daefe56b937974a765e39553f8
Moises Gomes <moisesgomes376@gmail.com>
2022-07-04 17:13:38 -0300

    Change PUT for PATCH

--- back/server.js
+++ back/server.js
@@ -3,9 +3,8 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  console.log(req.body);
   res.set({
-   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "X-Requested-With,content-type, Authorization"
   });
@@ -16,75 +15,101 @@ const port = 8001;
 const host = typeof process.argv[2] == "undefined" ? "localhost" : process.argv[2];
 app.listen(port, host, () => console.log(`API is running on 'http://${host}:${port}'`));
 
-let ID = 1;
-const author = {
-  id: ID,
-  name: "Moises Gomes",
-  age: 23,
-  city: "Niterói",
-};
-const users = [author];
+const users = [
+  {
+    id: 1,
+    name: "Moises Gomes",
+    age: 23,
+    city: "Niterói"
+  },
+  {
+    id: 2,
+    name: "Mons Drago",
+    age: 20,
+    city: "New York"
+  },
+  {
+    id: 3,
+    name: "Fabricio Ramos",
+    age: 28,
+    city: "Calcutá"
+  },
+  {
+    id: 4,
+    name: "Roberta Ribeiro",
+    age: 19,
+    city: "Londres"
+  }
+];
+let ID = users[users.length - 1].id;
 
 app.route("/").get((req, res) => res.send("<h1>Go to <a href='/users'>users</a></h1>"));
 
-app.route("/users").get((req, res) => res.send(users));
+app.route("/users").get((req, res) => res.json(users));
 
 app.route("/users/:id").get((req, res) => {
-  let responseSended = false;
-  users.forEach((user, index) => {
-    if (user.id == req.params.id) {
-      res.send(users[index]);
-      responseSended = true;
-    }
-  });
-  if (!responseSended) {
-    res.send("User not found :(");
+  const userID = req.params.id;
+  const user = users.find(user => Number(userID) === Number(user.id));
+  if (!user) {
+    return res.json("User not found :(");
   }
+  res.json(user);
 });
 
 app.route("/users").post((req, res) => {
   const newUser = {
     id: ++ID,
-    ...req.body
+    ...req.body,
+    age: Number(req.body.age)
   };
-  newUser.age = Number(newUser.age);
   users.push(newUser);
 
-  res.set({ "Content-type": "text/plain" });
-  res.send(`User ${newUser.name} created`);
+  res.json("New user created");
 });
 
-app.route("/users").put((req, res) => {
-  const body = req.body;
-  body.id = Number(body.id);
-  
-  const userIndex = users.findIndex(user => {
-    return user.id == body.id ? true : false;
-  });
-  if (userIndex == -1 || body.id <= 0) {
-    res.send("ID not found :(");
-    return;
+app.route("/users/:id").put((req, res) => {
+  const userID = req.params.id;
+
+  const user = users.find(user => Number(userID) === Number(user.id));
+  const userIndex = users.findIndex(user => Number(userID) === Number(user.id));
+
+  if (!user) {
+    return res.json("User not found :(");
   }
 
-  if (body.target == "age") {
-    body.newValue = Number(body.newValue);
-    if (isNaN(body.newValue) || body.newValue <= 0) {
-      res.send("Sorry, age is not right\nPlease be sure the 'age' field is a valid Number");
-      return;
-    }
-  }
-  users[userIndex][body.target] = body.newValue;
+  const updatedUser = {
+    ...user,
+    name: req.body.name,
+    age: req.body.age,
+    city: req.body.city
+  };
 
-  res.send(`${body.target} of user ${users[userIndex].name} updated!`);
+  users[userIndex] = updatedUser;
+  res.json("User updated");
+});
+
+app.route("/users/:id").patch((req, res) => {
+  const userID = req.params.id;
+
+  const userIndex = users.findIndex(user => Number(userID) === Number(user.id));
+  if (userIndex === -1) {
+    return res.json("User not found");
+  }
+
+  users[userIndex][req.body.target] = req.body.newValue;
+
+  res.json("Field updated");
 });
 
 app.route("/users/:id").delete((req, res) => {
-  const reqUserID = Number(req.params.id);
-  const userIndex = users.findIndex(user => reqUserID == user.id ? true : false);
-  if (userIndex <= -1) {
-    res.send("User id not found :(");
-    return;
+  const userID = req.params.id;
+
+  const userIndex = users.findIndex(user => Number(userID) === Number(user.id));
+  if (userIndex === -1) {
+    return res.json("User not found :(");
   }
-  res.send(`User ${users[userIndex].name} deleted`);
+
   users.splice(userIndex, 1);
+
+  res.json("User deleted");
 });
